7bad22841fb45cabc84bd0d622f8988cdc710b29
WentworthY <Wentworthy@outlook.com>
2019-11-28 18:33:40 +0800

    complete the grammar part

--- compiler/views.py
+++ compiler/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .my_func import lexer,grammar
+from .my_func import lexer, grammar
 from django.http import JsonResponse
 
 
@@ -9,7 +9,8 @@ def make_one_list(lex):
     len = lex.full_list()
     res = []
     for i in range(len):
-        demo = [i+1, lex.IDENTIFIER_LIST[i], lex.CHAR_LIST[i], lex.STRING_LIST[i], lex.CONSTANT_LIST[i], lex.K_LIST[i],
+        demo = [i + 1, lex.IDENTIFIER_LIST[i], lex.CHAR_LIST[i], lex.STRING_LIST[i], lex.CONSTANT_LIST[i],
+                lex.K_LIST[i],
                 lex.P_LIST[i],
                 ]
         res.append(demo)
@@ -24,10 +25,11 @@ def getLexerJson(request):
     table_list = make_one_list(lex)
     return JsonResponse({'res': res_list, 'table': table_list}, safe=False)
 
+
 def dealSrcFromWeb(src):
-    lex=lexer.Lexer()
+    lex = lexer.Lexer()
     lex.get_input_str(src)
-    res=lex.analyse(False)
+    res = lex.analyse(False)
     list_for_grammar = []
     for item in res:
         if item[0] == 'p':
@@ -37,23 +39,36 @@ def dealSrcFromWeb(src):
     list_for_grammar.append('#')
     return list_for_grammar
 
+
 def getReJson(request):
+    '''
+    递归子程序
+    :param request:
+    :return: acc or error
+    '''
     src = request.GET['src'].strip()
-    list_for_grammar=dealSrcFromWeb(src)
+    list_for_grammar = dealSrcFromWeb(src)
     path = "D:\CSE\jetbrains\pycharm\webforcomplier\compiler\my_func\grammar_static\grammar.txt"
-    re=grammar.RecursiveSubroutineFunc(path)
-    re.INPUT=list_for_grammar
-    res=re.analyse()
-    return JsonResponse({'res':res},safe=False)
+    re = grammar.RecursiveSubroutineFunc(path)
+    re.INPUT = list_for_grammar
+    res = re.analyse()
+    return JsonResponse({'res': res}, safe=False)
+
 
 def getLL1Json(request):
-    src=request.GET['src'].strip()
-    list_for_grammar=dealSrcFromWeb(src)
-    path="D:\CSE\jetbrains\pycharm\webforcomplier\compiler\my_func\grammar_static\grammar.txt"
-    ll_1=grammar.LL1(path)
+    '''
+    LL1分析法
+    :param request:
+    :return:select table
+    '''
+    src = request.GET['src'].strip()
+    list_for_grammar = dealSrcFromWeb(src)
+    path = "D:\CSE\jetbrains\pycharm\webforcomplier\compiler\my_func\grammar_static\grammar.txt"
+    ll_1 = grammar.LL1(path)
     ll_1.initAnalysisTable()
-    res=ll_1.analyzeInputString(list_for_grammar)
-    return  JsonResponse({'res':res},safe=False)
+    select=[list(item) for item in ll_1.SELECT]
+    res = ll_1.analyzeInputString(list_for_grammar)
+    return JsonResponse({'res': res, 'select': select, 'p_list': ll_1.P_LIST}, safe=False)
 
 
 def index(request):
